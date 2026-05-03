0575690b1740025d5d1e5e8798b89d9055995a99
Damian D'Souza <dsouzadamian189@gmail.com>
2026-05-03 21:19:16 +0200

    feat: add `urls` scraper action to extract links from a page

---
+++ app/Crawler/Scraper.hs
@@ -0,0 +1,11 @@
+module Crawler.Scraper where
+
+import Crawler.Types (URL)
+import Data.ByteString.Char8 as BS (pack)
+import Text.HTML.Scalpel (Scraper, attr, chroots)
+
+urls :: Scraper String [URL]
+urls = chroots "a" url
+  where
+    url :: Scraper String URL
+    url = BS.pack <$> attr "href" "a"
