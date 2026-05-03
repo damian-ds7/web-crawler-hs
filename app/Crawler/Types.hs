06416ae766dd1ecc0b141c6c5336cc3868c6fd31
Damian D'Souza <dsouzadamian189@gmail.com>
2026-05-03 14:30:18 +0200

    feat: implement CrawlerState type

---
+++ app/Crawler/Types.hs
@@ -0,0 +1,19 @@
+module Crawler.Types where
+
+import Control.Concurrent.STM (TMVar, TQueue)
+import Data.ByteString (ByteString)
+import Data.Map (Map)
+import Data.Set (Set)
+import GHC.Conc (TVar)
+import Network.HTTP.Robots (Robot)
+
+type URL = ByteString
+
+newtype Domain = Domain String deriving (Show, Eq, Ord)
+
+data CrawlerState = CrawlerState
+  { visitedURLs :: TVar (Set URL),
+    urlQueue :: TQueue URL,
+    robotsCache :: TVar (Map Domain (TMVar Robot)),
+    userAgent :: ByteString
+  }
