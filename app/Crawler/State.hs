fbacab85f420f1ccf326d78572934ee2b41bb84a
Damian D'Souza <dsouzadamian189@gmail.com>
2026-06-10 22:30:05 +0200

    feat: add blockedDomains set to `State`

--- app/Crawler/State.hs
+++ app/Crawler/State.hs
@@ -13,11 +13,13 @@ initState cfg seedURL = do
   visited <- newTVarIO Set.empty
   queue <- newTQueueIO
   robots <- newTVarIO Map.empty
+  blocked <- newTVarIO Set.empty
   atomically $ writeTQueue queue (seedURL, 0)
   return $
     State
       { visitedURLs = visited,
         urlQueue = queue,
         robotsCache = robots,
+        blockedDomains = blocked,
         config = cfg
       }
